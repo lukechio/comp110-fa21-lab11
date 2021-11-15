ec31b71720f2850c400a92367a38b4a8b3ff07a7
Luke Chiovare <lchiovare@sandiego.edu>
2021-11-15 14:54:15 -0800

    update

--- comp110_lab11.py
+++ comp110_lab11.py
@@ -18,21 +18,31 @@ def get_flash_cards(filename):
     """
 
     card_file = open(filename, 'r')
+    d = {}
 
     for line in card_file:
+        new_line = line.strip
+
         # Replace the following line with lines that split the line and add it to a dictionary
         # Use the `strip` function to ensure that the answer doesn't include a
         # newline character (i.e. '\n')
         pass
 
-    return None # modify this line to return your dictionary
+    return None  # modify this line to return your dictionary
 
 
 # To Do: Define your quiz function immediately AFTER this line.
 
 
 def main():
-    pass # remove this line when implementing main
+
+    print("Welcome to Flash in the pan, a Flash Card Program")
+
+    input("Enter the name of the file would like to see flashcards for")
+
+    input("Enter the score you would like to target on this quiz")
+
+    print(percentage_score)
 
 
 """ DO NOT MODIFY ANYTHING BELOW THIS LINE! """
