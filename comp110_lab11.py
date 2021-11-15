edbabcd6dff3bb4b8d5e2648dc45aa7ad7d332b2
Luke Chiovare <lchiovare@sandiego.edu>
2021-11-15 15:24:58 -0800

    update

--- comp110_lab11.py
+++ comp110_lab11.py
@@ -20,15 +20,15 @@ def get_flash_cards(filename):
     card_file = open(filename, 'r')
     d = {}
 
-    my_dict = {}
-
     for line in card_file:
-        new_line = line.strip
-
         # Replace the following line with lines that split the line and add it to a dictionary
         # Use the `strip` function to ensure that the answer doesn't include a
         # newline character (i.e. '\n')
-        pass
+        fields = line.string().split("|")
+        eng = fields[0]
+        esp = fields[1]
+        d[eng] = esp
+        return d
 
 
 def quiz(cards, target_score):
@@ -40,7 +40,21 @@ def quiz(cards, target_score):
     Returns: Total number of questions it tok for the user to reach their target score
     """
 
-    return None  # modify this line to return your dictionary
+    questions = list(cards.keys())
+    score = 0
+    num_tries = 0
+    while score < target_score:
+        num_tries += 1
+        q = random.choice(questions)
+        a = cards[q]
+        print(q)
+        response = input('Answer: ')
+        if response == a:
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
+    return num_tries
 
 
 # To Do: Define your quiz function immediately AFTER this line.
@@ -53,13 +67,13 @@ def main():
     Returns : what percentage they got on the quiz
     """
 
-    print("Welcome to Flash in the pan, a Flash Card Program")
-
-    input("Enter the name of the file would like to see flashcards for")
-
-    input("Enter the score you would like to target on this quiz")
-
-    print()
+    print("Welcome to Flash dance, an amazing Flash Card app")
+    fc_filename = input("Enter the name of the flash card file: ")
+    cards = get_flash_cards(fc_filename)
+    target = int(input("Enter your target score"))
+    tries = quiz(cards, target)
+    rate = (target/tries) * 100
+    print("You got the following percent right: ", rate)
 
 
 """ DO NOT MODIFY ANYTHING BELOW THIS LINE! """
